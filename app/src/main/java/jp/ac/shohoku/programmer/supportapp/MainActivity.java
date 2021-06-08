e1049665b7183930bbd5a64154cca9b4f02819a7
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-06-08 10:47:24 +0900

    画面遷移の作成(途中)

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -1,14 +1,22 @@
 package jp.ac.shohoku.programmer.supportapp;
 
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+    //ボタンが押された時の処理
+    public void onClick(View view){
+        Intent intent = new Intent(this, MainActivity2.class);  //インテントの作成
+        startActivity(intent);                                 //画面遷移
     }
 }
