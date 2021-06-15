642e569d95d0593dd25ace09dc974c72e3d0e05c
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-06-15 10:05:18 +0900

    ボタンの配置

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -4,18 +4,31 @@ import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(this);
+
+        Button nextButton = findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
     //ボタンが押された時の処理
-    public void onClick(View view){
+    public void onClick(View view) {
         Intent intent = new Intent(this, MainActivity2.class);  //インテントの作成
         startActivity(intent);                                 //画面遷移
     }
