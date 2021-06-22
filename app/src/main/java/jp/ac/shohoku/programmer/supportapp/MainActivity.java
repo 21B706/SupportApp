2c9858335cb446f4e7c338c9d193ff0f9f69b8db
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-06-22 10:48:13 +0900

    画面遷移(途中)

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -1,34 +1,31 @@
 package jp.ac.shohoku.programmer.supportapp;
 
+import android.app.Activity;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button nextButton = findViewById(R.id.button);
-//        sendButton.setOnClickListener(v -> {
-//            Intent intent = new Intent(getApplication(), MainActivity2.class);
-//            startActivity(intent);
-//        });
-
-       nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), MainActivity2.class);
-                startActivity(intent);
-            }
-        });
-
-
+    }
+    public void onClick(View v){
+        //インテントの作成
+        Intent intent = new Intent(this, MainActivity2.class);
+       //データをセット
+        EditText editText = (EditText)this.findViewById(R.id.Button);
+        intent.putExtra("sendText",editText.getText().toString());
+        //遷移先の画面を起動
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
@@ -1,8 +1,24 @@
 package jp.ac.shohoku.programmer.supportapp;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
+    private Bundle savedInstanceState;
+
+    @Override
+    protected void onCreate(Bundle saveInstanseState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_main);
+        
+        Intent intent = this.getIntent();
+        String text = intent.getStringExtra("sendText");
+        TextView textView = (TextView)this.findViewById(R.id.Button);
+        textView.setText(text);
+    }
 }
