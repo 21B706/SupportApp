a3b07abac14a7e033b96ba680a06fbd763daf75c
s20b706 <s20b706@shohoku.ac.jp>
2021-06-29 10:40:46 +0900

    ボタンの配置固定

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -1,31 +1,35 @@
 package jp.ac.shohoku.programmer.supportapp;
 
-import android.app.Activity;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button Button = findViewById(R.id.Button);
+        Button nextButton = findViewById(R.id.Button);
+//        sendButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(getApplication(), MainActivity2.class);
+//            startActivity(intent);
+//        });
 
-        Button button1 = findViewById(R.id.Button);
-        button1.setOnClickListener(new View.OnClickListener(){
+       nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getApplication(),MainActivity2.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
                 startActivity(intent);
             }
             
         });
+
+
     }
+
 }
