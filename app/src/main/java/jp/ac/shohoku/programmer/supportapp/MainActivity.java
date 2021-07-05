95b9e1f0038ba57c86acedba9a55571423883edd
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-07-06 00:11:23 +0900

    画面遷移とレイアウトの修正

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -15,10 +15,14 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button nextButton = findViewById(R.id.Button);
-//        sendButton.setOnClickListener(v -> {
-//            Intent intent = new Intent(getApplication(), MainActivity2.class);
-//            startActivity(intent);
-//        });
+        Button nextButton2 = findViewById(R.id.button2);
+        Button nextButton3 = findViewById(R.id.button3);
+        Button nextButton4 = findViewById(R.id.button4);
+        Button nextButton5 = findViewById(R.id.button5);
+        Button nextButton6 = findViewById(R.id.button6);
+        Button nextButton7 = findViewById(R.id.button7);
+        Button nextButton8 = findViewById(R.id.button8);
+        Button nextButton9 = findViewById(R.id.button9);
 
        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +30,62 @@ public class MainActivity extends AppCompatActivity  {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
                 startActivity(intent);
             }
-            
+        });
+        nextButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        nextButton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity2.class);
+                startActivity(intent);
+            }
         });
 
 
--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
@@ -2,14 +2,25 @@ package jp.ac.shohoku.programmer.supportapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2_main);
+        Button ReturnButton = findViewById(R.id.button10);
+
+        ReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
