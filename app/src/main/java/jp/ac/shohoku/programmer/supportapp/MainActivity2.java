e218011c6d45842d51ed7fa31007b6c42f945014
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-07-06 11:35:22 +0900

    画面遷移とレイアウトの修正

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
@@ -13,8 +13,16 @@ public class MainActivity2 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2_main);
         Button ReturnButton = findViewById(R.id.button10);
+        Button ConfirmButton = findViewById(R.id.button11);
 
         ReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        ConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity.class);
