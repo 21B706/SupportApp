592f93a8d701e10a36f3d876edc366e3126f4240
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2021-07-20 10:38:44 +0900

    画面遷移とレイアウトの修正

--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
@@ -24,66 +26,93 @@ public class MainActivity extends AppCompatActivity  {
         Button nextButton8 = findViewById(R.id.button8);
         Button nextButton9 = findViewById(R.id.button9);
 
+        //教職員からの問い合わせ
        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","teacher,inquiry");
                 startActivity(intent);
             }
         });
+        //学生からの問い合わせ
         nextButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","student,inquiry");
                 startActivity(intent);
             }
         });
+        //その他からの問い合わせ
         nextButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","other,inquiry");
                 startActivity(intent);
             }
         });
+        //学生からの障害
         nextButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","student,Disability");
                 startActivity(intent);
             }
         });
+        //教職員からの障害
         nextButton5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","teacher,Disability");
                 startActivity(intent);
             }
         });
+        //その他からの障害
         nextButton6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","other,Disability");
                 startActivity(intent);
             }
         });
+        //教職員ーサポート
         nextButton7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","teacher,support");
                 startActivity(intent);
             }
         });
+        //学生ーサポート
         nextButton8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","student,support");
                 startActivity(intent);
             }
         });
+        //その他ーサポート
         nextButton9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                EditText editText = findViewById(R.id.editText);
+                intent.putExtra("category","other,support");
                 startActivity(intent);
             }
         });
--- app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
+++ app/src/main/java/jp/ac/shohoku/programmer/supportapp/MainActivity2.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +17,96 @@ public class MainActivity2 extends Activity {
         Button ReturnButton = findViewById(R.id.button10);
         Button ConfirmButton = findViewById(R.id.button11);
 
+        //Button(教員、問い合わせ)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent = getIntent();
+        String getdata = intent.getStringExtra("category");
+        //値をセット
+        TextView textView = findViewById(R.id.Button);
+        textView.setText(getdata);
+
+        //Button(学生、問い合わせ)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent2 = getIntent();
+        String getdata2 = intent2.getStringExtra("category");
+        //値をセット
+        TextView textView2 = findViewById(R.id.button2);
+
+        //Button(その他、問い合わせ)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent3 = getIntent();
+        String getdata3 = intent3.getStringExtra("category");
+        //値をセット
+        TextView textView3 = findViewById(R.id.button3);
+        textView3.setText(getdata3);
+
+        //Button(学生、障害)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent4 = getIntent();
+        String getdata4 = intent4.getStringExtra("category");
+        //値をセット
+        TextView textView4 = findViewById(R.id.button4);
+        textView4.setText(getdata4);
+
+        //Button(教職員、障害)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent5 = getIntent();
+        String getdata5 = intent5.getStringExtra("category");
+        //値をセット
+        TextView textView5 = findViewById(R.id.button5);
+        textView5.setText(getdata5);
+
+        //Button(その他、障害)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent6 = getIntent();
+        String getdata6 = intent6.getStringExtra("category");
+        //値をセット
+        TextView textView6 = findViewById(R.id.button6);
+        textView6.setText(getdata6);
+
+        //Button(教職員、支援)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent7 = getIntent();
+        String getdata7 = intent7.getStringExtra("category");
+        //値をセット
+        TextView textView7 = findViewById(R.id.button7);
+        textView7.setText(getdata7);
+
+        //Button(学生、支援)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent8 = getIntent();
+        String getdata8 = intent8.getStringExtra("category");
+        //値をセット
+        TextView textView8 = findViewById(R.id.button8);
+        textView8.setText(getdata8);
+
+        //Button(その他、支援)
+        //activity_subレイアウトをセット
+        setContentView(R.layout.activity2_main);
+        //MAINからの値を取得
+        Intent intent9 = getIntent();
+        String getdata9 = intent9.getStringExtra("category");
+        //値をセット
+        TextView textView9 = findViewById(R.id.button9);
+        textView9.setText(getdata9);
+
+        //リセットボタン
         ReturnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +114,7 @@ public class MainActivity2 extends Activity {
                 startActivity(intent);
             }
         });
+        //確定ボタン
         ConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
